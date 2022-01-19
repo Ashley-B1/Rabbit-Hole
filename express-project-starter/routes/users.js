35390b835882311b03e244899a9c802bb95b9ec7
Hamlet <hamlet.villa@gmail.com>
2022-01-19 12:57:47 -0500

    user password hashing added, edits

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const { asyncHandler } = require('../middleware/error-handling');
+const csrf = require('csurf');
+const csrfProtection = csrf({cookie: true});
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
-router.get('/user/register', csrfProtection, asyncHandler(async(req, res) => {
+router.get('/user/create', csrfProtection, asyncHandler(async(req, res) => {
   const user = db.User.build();
-  res.render('user-register', {
+  res.render('create-user', {
     title: 'New User',
     user,
     csrfToken: req.csrfToken(),
@@ -17,17 +21,19 @@ router.get('/user/register', csrfProtection, asyncHandler(async(req, res) => {
 
 const userValidators = [];
 
-router.post('user/register', csrfProtection, userValidators, asyncHandler(async(req, res) => {
-  const { firstName, lastName, emailAddress, password } = req.body;
-  const user = db.User.build({ firstName, lastName, emailAddress });
+router.post('user/create', csrfProtection, userValidators, asyncHandler(async(req, res) => {
+  const { userName, firstName, lastName, email, password } = req.body;
+  const user = db.User.build({ userName, firstName, lastName, email });
   const validatorErrors = userValidators(req);
 
-  if(!validatorErrors){
+  if(validatorErrors.isEmpty()){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
     await user.save();
     res.redirect('/');
   } else {
     const errors = validatorErrors.array().map(error => error.msg);
-    res.render('user-register', { title: 'New User', user, errors, csrfToken: req.csrfToken() });
+    res.render('create-user', { title: 'New User', user, errors, csrfToken: req.csrfToken() });
   }
 }));
 
