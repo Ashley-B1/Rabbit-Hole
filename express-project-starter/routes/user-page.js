c1f1067ef45870cdfac8b898794421ffea56b6f3
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-01-19 16:45:53 -0500

    Connect database with route in user-page.js

--- express-project-starter/routes/user-page.js
+++ express-project-starter/routes/user-page.js
@@ -15,21 +15,30 @@ router.route('/:id(\\d+)')
 .get(asyncHandler(async(req, res) => {
   const userId = parseInt(req.params.id, 10);
 
-  // const likesCount = await db.PostLike.findAll({where: postId}).length;
-  
-  // const comments = await db.Comment.findAll({where: postId});
-  // const commentsCount = comments.length;
+  const likesCount = await db.PostLike.findAll({where: userId}).length || `There are no posts. Add seeder data?`;
+  console.log(`likes count: ${likesCount}`)
 
-  // const posts = await db.Post.findAll({
-  //   order: [["createdAt", "DESC"]],
-  //   // include: [{ model: User, as: "user", attributes: ["username"] }],
-  // });
+  const comments = await db.Comment.findAll({where: userId}) || `There are no posts. Add seeder data?`;
+  console.log(`comments: ${comments}`)
+  const commentsCount = comments.length || `There are no posts. Add seeder data?`;
+  console.log(`commentsCount: ${commentsCount}`)
+
+  const posts = await db.Post.findAll({
+    order: [["createdAt", "DESC"]],
+    // include: [
+    //   { model: PostLike, as: "user", attributes: ["username"] },
+    //   { model: Comment, as: "user", attributes: ["username"] },
+    // ],
+  }) || `There are no posts. Add seeder data?`;
+  console.log(`posts: ${posts}`)
 
 
   res.render('user-page', {
-    // posts
     userName: userId,
+    posts,
     followersCount: 1,
+    likesCount,
+    commentsCount,
   })
 }));
 
