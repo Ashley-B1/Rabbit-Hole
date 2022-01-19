5743bc4ae08e448a73987a1d512d8fa1cc6baeb4
Hamlet <hamlet.villa@gmail.com>
2022-01-19 16:52:14 -0500

    user login routes and template

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const express = require('express');
 const router = express.Router();
 
@@ -6,4 +7,45 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+
+router.get('user/login', csrfProtection, (req, res) => {
+  res.render('user-login', { title: 'Login', csrfToken: req.csrfToken() });
+});
+
+const loginValidators = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter a valid password.')
+]
+
+router.post('user/login', csrfProtection, loginValidators, asyncHandler(async(req, res) => {
+  const { email, password } = req.body;
+
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if(validatorErrors.isEmpty()){
+    const user = await db.User.findOne({ where: { email }});
+    if(user !== null) {
+      const passWordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+      if(passWordMatch) {
+        // TODO: login user
+        return res.redirect('/');
+      }
+    }
+    errors.push('Login failed. Invalid email or password provided.')
+  } else {
+    errors = validatorErrors.array().map(error => error.msg);
+  }
+
+  res.render('user-login', {
+    title: 'Log In',
+    email,
+    errors,
+    csrfToken: req.csrfToken()
+  });
+}));
 module.exports = router;
