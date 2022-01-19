47039e255b00fa2e4b603db18748fa51ce4ff4e3
Wanyi <wanyilee0805@gmail.com>
2022-01-19 13:23:01 -0800

    POST /post/create route, still need to modify the userId in query

--- express-project-starter/routes/post.js
+++ express-project-starter/routes/post.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator')
 const router = express.Router();
 
 const db = require('../db/models');
@@ -10,6 +11,8 @@ const csrfProtection = csrf({ cookie: true });
 // need to confirm the directory
 // const { asyncHandler } = require('./')
 
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
 // need to add asyncHandler
 router.get('/posts/create', csrfProtection, async(req, res) => {
   const post = db.Post.build();
@@ -19,6 +22,47 @@ router.get('/posts/create', csrfProtection, async(req, res) => {
     csrfToken: req.csrfToken()
   })
 })
+
+const postValidators = [
+  check('title')
+    .exists({ checkFalsy: true})
+    .withMessage('Title cannot be empty.')
+    .isLength({ max: 255 })
+    .withMessage('Title must be less than 255 characters.'),
+  check('content')
+    .exists({ checkFalsy: true})
+    .withMessage('Content cannot be empty.')
+
+]
+
+
+router.post('/posts/create', postValidators, csrfProtection, asyncHandler(async(req, res) => {
+  const {
+    title,
+    content
+  } = req.body
+
+  console.log('req.body is HERE!!')
+  const post = db.Post.build({
+    // userId: res.locals.user.id,
+    title,
+    content
+  })
+  const validationErrors = validationResult(req);
+
+  if (validationErrors.isEmpty()) {
+    await post.save();
+    res.redirect('/'); // maybe redirect to user profile page
+  } else {
+    const errors = validationErrors.array().map((error) => error.msg);
+    res.render('create-post', {
+      title: 'Add New Story',
+      post,
+      errors,
+      csrfToken: req.csrfToken()
+    })
+  }
+}))
 
 
 module.exports = router
