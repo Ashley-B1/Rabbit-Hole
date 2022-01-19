9f972ff64c07701a1138293bd9724eae4f3c357a
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-01-19 17:28:15 -0500

    Remove 'allowNull: false' in 'updatedAt' column for all migration files

--- express-project-starter/db/seeders/20220119221730-User-seed-data.js
+++ express-project-starter/db/seeders/20220119221730-User-seed-data.js
@@ -1,26 +1,9 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [
+      { userName: 'Demo', firstName: 'Demo', lastName: 'User', email: 'demo@user.com', password: 'mediumpassword'},
+  ], {}),
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {})
 };
