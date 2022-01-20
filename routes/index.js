0c856b8d3d1db109b0d935acfe65148cde4507d2
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-01-19 20:19:30 -0500

    Move all files in project skeleton to root repository

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('home-page', { title: 'a/A Express Skeleton Home' });
+});
+
+module.exports = router;
