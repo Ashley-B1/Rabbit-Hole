51be597732ec6838144e7ceed098811a6a54b959
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-01-20 15:58:12 -0500

    Reformat all seed data

--- db/seeders/20220119221730-User-seed-data.js
+++ db/seeders/20220119221730-User-seed-data.js
@@ -11,9 +11,30 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
-      { userName: 'Tom8-o', firstName: 'Thomas', lastName: 'Aytoe', email: 'tom8o@anthropomorphicVeg.org', password: 'iAmFruit', createdAt: new Date(), updatedAt: new Date()},
-      { userName: 'waitWhoAmIAgain', firstName: 'Finding', lastName: 'Dorian', email: 'dory@worldwildlifefund.org', password: 'PSherman42WallabyWay', createdAt: new Date(), updatedAt: new Date()},
-      { userName: 'thirdGhastlySonOfCharlesDickens', firstName: 'Gustov', lastName: 'ChristmasPast', email: 'gcpchristmasCarol@booksTheyMadeIntoMovies.com', password: 'Carol1843', createdAt: new Date(), updatedAt: new Date()},
+      { userName: 'Tom8-o',
+        firstName: 'Thomas',
+        lastName: 'Aytoe',
+        email: 'tom8o@anthropomorphicVeg.org',
+        password: 'iAmFruit',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      { userName: 'waitWhoAmIAgain',
+      firstName: 'Finding',
+      lastName: 'Dorian',
+      email: 'dory@worldwildlifefund.org',
+      password: 'PSherman42WallabyWay',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    { userName: 'thirdGhastlySonOfCharlesDickens',
+      firstName: 'Gustov',
+      lastName: 'ChristmasPast',
+      email: 'gcpchristmasCarol@booksTheyMadeIntoMovies.com',
+      password: 'Carol1843',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
   ], {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {})
