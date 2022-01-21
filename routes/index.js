3b33df18da87355d3af874d2872c0fd211e76efd
Ashley-B1 <stormi.seuneu@outlook.com>
2022-01-21 11:18:30 -0600

    added article photos and created top articles section

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const db = require('../db/models');
 
 router.route('/')
 .get(asyncHandler(async(req, res) => {
-  const posts = await db.Post.findAll({ limit: 4 })
+  const posts = await db.Post.findAll({ limit: 6 })
 
   console.log(posts);
   res.render('home-page', { title: 'rabbit hole', posts })
