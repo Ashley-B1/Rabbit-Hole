ba607b86d0fc7e21c62481705438eef624d8b8ce
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-01-23 16:23:21 -0500

    Done?

--- routes/posts.js
+++ routes/posts.js
@@ -279,15 +279,15 @@ router.get('/:id/comments/:commentId/delete', csrfProtection, asyncHandler(async
 }))
 
 // delete the comment
-// router.post('/:id/comments/:commentId/delete', csrfProtection, asyncHandler(async(req, res) => {
-//   const commentId = parseInt(req.params.commentId, 10);
-//   const comment = await db.Comment.findByPk(commentId);
-//   const postId = parseInt(req.params.id, 10);
-//   console.log("comment!!!!!!", comment);
-//   console.log("postID!!!!", postId)
-//   await comment.destroy();
-//   res.redirect(`/posts/${postId}`);
-// }));
+router.post('/:id/comments/:commentId/delete', csrfProtection, asyncHandler(async(req, res) => {
+  const commentId = parseInt(req.params.commentId, 10);
+  const comment = await db.Comment.findByPk(commentId);
+  const postId = parseInt(req.params.id, 10);
+  console.log("comment!!!!!!", comment);
+  console.log("postID!!!!", postId)
+  await comment.destroy();
+  res.redirect(`/posts/${postId}`);
+}));
 
 const commentNotFoundError = (commentId) => {
   const err = new Error(`Comment ID #${commentId} does not exist.`);
